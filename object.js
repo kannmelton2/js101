2f16ae7d2d060afd6be268e4990f64f95f493ad4
Kayla Melton <kannmelton2@gmail.com>
2020-01-20 20:45:28 -0600

    completed objects challenges

--- object.js
+++ object.js
@@ -18,3 +18,36 @@ const ageCheck = age => {
 
 ageCheck(12);
 ageCheck(24);
+
+const expense = {
+   dateCreated: '01/17/2020',
+   location: 'Baja Burrito',
+   cost: 12.57,
+   isGood: true
+};
+
+console.log(expense['cost']);
+
+const employee = {
+    firstName: 'Achilles',
+    lastName: 'Melton',
+    job: 'Giver of Love and Puppy Kisses',
+    industry: 'happiness',
+    startDate: '04/18/2015',
+    payment: 'two meals, plus treats and a daily walk',
+    likesToCuddle: true
+};
+
+const statusMaker = (employeeStatus) => {
+    if (employeeStatus.firstName[0] === 'M') {
+        employeeStatus.status = 'vip';
+    }
+    else {
+        employeeStatus.status = 'peasant';
+    }
+    return employeeStatus;
+}
+statusMaker(employee);
+console.log(employee);
+
+
